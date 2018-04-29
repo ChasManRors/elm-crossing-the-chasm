8e0b1e9dcd4a1e15134c31205422edd3cf99e22e
Charles Magid <cmagid@rbmtechnologies.com>
2018-04-29 07:59:55 -0400

    testing if first letters are the same

---
+++ Exercise21.elm
@@ -0,0 +1,20 @@
+module Exercise21 exposing (..)
+
+import Html exposing (..)
+
+
+(~=) a b =
+    (String.left 1 a) == (String.left 1 b)
+
+
+result a b =
+    a ~= b
+
+a =
+    "Blake"
+b =
+    "Barney"
+
+
+main =
+    Html.text ("The first letter of the names " ++ a ++ " and " ++ b ++ " are the same: " ++ (toString (result a b)))
