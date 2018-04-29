7c617b398db69030f3d2c592d211fd94352d6ae2
Charles Magid <cmagid@rbmtechnologies.com>
2018-04-29 06:48:30 -0400

    Better working add function

--- Main.elm
+++ Main.elm
@@ -2,12 +2,14 @@ module Login exposing (..)
 
 import Html exposing (..)
 
+
 add a b =
     a + b
+
 
 result =
     add 1 2
 
 
 main =
-    Html.text (toString result)
+    toString result |> Html.text
