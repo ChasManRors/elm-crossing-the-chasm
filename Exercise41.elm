d61239e135af7afbad5e2cdc0337bcdbaf4b3735
Charles Magid <cmagid@rbmtechnologies.com>
2018-04-29 18:58:23 -0400

    My try at a better calorie couter

---
+++ Exercise41.elm
@@ -0,0 +1,95 @@
+module Exercise41 exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+-- model
+
+
+type alias Model =
+    { calories : Int
+    , input : Int
+
+    -- , error : Maybe String
+    }
+
+
+initModel : Model
+initModel = Model 0 0
+    -- { calories = 0
+    -- , input = 0
+
+    -- -- , error = Nothing
+    -- }
+
+
+
+-- update
+
+
+type Msg
+    = AddCalorie
+    | Input String
+    | Clear
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        AddCalorie ->
+            { model
+                | calories = model.calories + model.input
+                , input = 0
+            }
+
+        Input val ->
+            { model | input = (String.toInt val |> Result.withDefault 0) }
+
+        Clear ->
+            initModel
+
+
+
+-- view
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h3 []
+            [ text ("Total Calories: " ++ (toString model)) ]
+        , p [ class "story" ]
+            [ text "Once" ]
+        , input
+            [ type_ "text"
+            , onInput Input
+            , value
+                (if model.input == 0 then
+                    ""
+                 else
+                    toString model.input
+                )
+            ]
+            []
+        , button
+            [ type_ "button"
+            , onClick AddCalorie
+            ]
+            [ text "Add" ]
+        , button
+            [ type_ "button"
+            , onClick Clear
+            ]
+            [ text "Clear" ]
+        ]
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { model = initModel
+        , update = update
+        , view = view
+        }
